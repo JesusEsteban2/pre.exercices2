98d1780aa91c0eaa8b9c5743994a865492f1c02c
jesusesteban2 <jesus.esteban@outlook.com>
2024-11-14 19:23:16 +0100

    Update 10

--- index.js
+++ index.js
@@ -134,21 +134,40 @@ function checkRollDice() {
   return result;
 }
 
+console.dir('Ejercicio 9');
 rollDice(6);
 rollDice(10);
 console.table(checkRollDice());
 
 // 10-Crea una función que reciba un año por parámetros y compruebe e imprima por consola si el año es bisiesto o no.
 
+function isLeapYear(year = 0) {
+  if (year % 4 === 0 && (!year % 100 === 0 || year % 400 === 0)) {
+    return true;
+  }
+
+  return false;
+}
+
+console.dir('Ejercicio 10');
+console.dir(isLeapYear(2024));
+console.dir(isLeapYear(2025));
+
 // 11-Escribe una función que simula el juego piedra, papel y tijera. Recibirá como parámetro una opción (piedra, papel o tijera) en forma de string. La máquina, elegirá automáticamente una opción aleatoria. Imprime por consola ambas elecciones y en caso de ganar el jugador un mensaje de victoria, y en caso de perder uno de derrota.
 
+console.dir('Ejercicio 11');
 // 12-La serie de Fibonacci es un problema matemático que realiza la suma de los dos números anteriores para generar el siguiente. Crea una función que imprima por consola la serie de Fibonacci hasta un número introducido por el usuario. El usuario debe ser preguntado por este número al iniciar la aplicación.
 
+console.dir('Ejercicio 12');
 // 13-Escribe una función generadora de nombres de usuario aleatorios, a partir de dos grupos de palabras dadas. Estos grupos de palabras pueden estar agrupados en arrays. (nombres=['Hugo', 'Luis'], apellidos=['Duro', 'Fabiano']). Retorna un nombre de usuario aleatorio con nombre, apellido y un número aleatorio del 1 al 100. (Por ejemplo -> 'Pepe Pérez 87'.)
 
+console.dir('Ejercicio 13');
 // 14-Crea una función calculadora de propinas. Debe recibir el total de la cuenta y el porcentaje de propina deseado, con ello deberá calcular e imprimir por consola la cuenta, la propina que corresponde a la cuenta introducida, y el total a pagar. Redondea a dos decimales.
 
+console.dir('Ejercicio 14');
 // 15-Escribe una función que calcule el descuento aplicado a un precio. La función recibirá el precio y el descuento del artículo en venta, con ellos deberá calcular e imprimir por consola el precio, el descuento y el total del precio una vez aplicado el descuento. Redondea a dos decimales.
+
+console.dir('Ejercicio 15');
 
 // Prueba de String.normalize()
 
